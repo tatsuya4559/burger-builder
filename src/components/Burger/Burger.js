b53c37fd4e7f78a3eacfa2182d4fd6a074071b90
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-01-16 23:38:01 +0900

    feat 160 具材の状態をpropsで通した

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,33 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
 const Burger = (props) => {
+  // let transformedIngredients = Object.keys(props.ingredients)
+  //   .map(key => {
+  //     return [...Array(props.ingredients[key])].map((_, i) => (
+  //       <BurgerIngredient key={key + i} type={key} />
+  //     ));
+  //   })
+  //   .reduce((acc, e) => {
+  //     return acc.concat(e)
+  //   }, []);
+
+  // map, reduceがわかりにくいからforにしてみた
+  // このほうがわかりやすいが、pythonは更にシンプルになった！
+  let transformedIngredients = [];
+  Object.entries(props.ingredients).forEach(([ingrediennt, amount]) => {
+    for (let i = 0; i < amount; i++) {
+      transformedIngredients.push(<BurgerIngredient key={ingrediennt + i} type={ingrediennt} />);
+    }
+  });
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
