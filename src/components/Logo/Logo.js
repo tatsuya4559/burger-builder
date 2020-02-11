9ee179565a9a8c2963258dce1fa7f9556d1f1314
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-02-11 10:23:57 +0900

    263 section12 completed

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import burgerLogo from '../../assets/image/burger-logo.png';
 import classes from './Logo.module.css';
 
 const Logo = (props) => {
+  const handleClicked = () => {
+    props.history.push('/');
+  }
+
   return (
     <div className={classes.Logo} style={{height: props.height}}>
-      <img src={burgerLogo} alt="MyBurger" />
+      <img onClick={handleClicked} src={burgerLogo} alt="MyBurger" />
     </div>
   );
 };
 
-export default Logo;
+export default withRouter(Logo);
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,9 +3,25 @@ import React from 'react';
 import classes from './Order.module.css';
 
 const Order = (props) => {
+  let ingredients = [];
+  Object.entries(props.ingredients).forEach(([ingredient, amount]) => {
+    ingredients.push(
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: '0 8px',
+          padding: '5px',
+          border: '1px solid #ccc'
+        }}
+        key={ingredient}>{ingredient} ({amount})</span>
+    );
+  });
+
   return (
     <div className={classes.Order}>
-      <p>ORDER</p>
+      <p>Ingredients: {ingredients}</p>
+      <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
     </div>
   );
 };
