1128161110d1d368f1a2289729548ffea930c306
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-02-26 22:29:27 +0900

    import順を整理

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-
-import Layout from './containers/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import Layout from "./containers/Layout/Layout";
+import Orders from "./containers/Orders/Orders";
 
 function App() {
   return (
